0b770bbef22955369ff5c8e501867e81d8f182fc
Hachem "Timeless" Bouhadede <hachembouhadede@gmail.com>
2023-09-22 18:07:27 +0200

    completed assigment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,7 +2,7 @@
 
 function Keypad (){
     return (
-        <div></div>
+        <input type ="password" placeholder="Enter Password" onChange={(e) => {console.log('Entering password...')}}/>      
     )
 }
 
